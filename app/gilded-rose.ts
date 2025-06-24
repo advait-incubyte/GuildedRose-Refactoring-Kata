0617aee12052510883b60ebda6cfbdc925fdc90f
Advait Maindalkar <advait@incubyte.co>
2025-06-24 21:32:26 +0530

    refactor: extracted each switch case logic into separate functions

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,38 +21,51 @@ export class GildedRose {
     for (const item of this.items) {
       switch (item.name) {
         case 'Sulfuras, Hand of Ragnaros':
-          break
+          break;
 
         case 'Aged Brie':
-          item.sellIn -= 1
-          item.quality += (item.sellIn < 0 ? 2 : 1)
-          item.quality = Math.min(item.quality, 50)
-          break
+          this._updateQualityofAgedBrie(item);
+          break;
 
         case 'Backstage passes to a TAFKAL80ETC concert':
-          item.sellIn -= 1
-
-          if (item.sellIn < 0) {
-            item.quality = 0
-          } else {
-            item.quality += 1
-            if (item.sellIn <= 10)
-              item.quality += 1
-            if (item.sellIn <= 5)
-              item.quality += 1 
-
-            item.quality = Math.min(item.quality, 50)
-          }
-          break
+          this._updateQualityOfBackstagePass(item);
+          break;
 
         default:
-          item.sellIn -= 1
-          item.quality -= (item.sellIn < 0 ? 2 : 1)
-          item.quality = Math.max(item.quality, 0)
-          break
+          this._updateQualityOfNormalItem(item)
+          break;
       }
     }
-
+  
     return this.items;
+  }
+
+  private _updateQualityOfNormalItem(item: Item) {
+    item.sellIn -= 1
+    item.quality -= (item.sellIn < 0 ? 2 : 1)
+    item.quality = Math.max(item.quality, 0)
+  }
+
+  private _updateQualityOfBackstagePass(item: Item) {
+    item.sellIn -= 1;
+
+    if (item.sellIn < 0) {
+      item.quality = 0;
+      return
+    }
+
+    item.quality += 1;
+    if (item.sellIn <= 10)
+      item.quality += 1;
+    if (item.sellIn <= 5)
+      item.quality += 1;
+
+    item.quality = Math.min(item.quality, 50);
+  }
+
+  private _updateQualityofAgedBrie(item: Item) {
+    item.sellIn -= 1
+    item.quality += (item.sellIn < 0 ? 2 : 1)
+    item.quality = Math.min(item.quality, 50)
   }
 }
